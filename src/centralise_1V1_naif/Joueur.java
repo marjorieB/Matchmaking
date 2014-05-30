e000a411dc0effef615769cbf2e207d845782b8d
marjorieB <marjorie.bournat@wanadoo.fr>
2014-05-30 11:26:29 +0200

    avec nombre de threads paramètrables

--- src/centralise_1V1_naif/Joueur.java
+++ src/centralise_1V1_naif/Joueur.java
@@ -20,7 +20,8 @@ public class Joueur {
 			fr = new FileReader("../../M1_SAR/stage_M1/joueurs_proprietes.csv");
 			br = new BufferedReader(fr);
 			int i = 0;
-			while ((lu = br.readLine()) != null && i < 200) {
+			int nb_threads = Integer.parseInt(arg[0]);
+			while ((lu = br.readLine()) != null && i < nb_threads) {
 				proprietes = lu.split(",");
 				summonerElo = Integer.parseInt(proprietes[1]);
 				latency = (int)Double.parseDouble(proprietes[2]);
