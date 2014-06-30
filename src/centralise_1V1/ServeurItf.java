1879e8d975dbe14da393e3bf175b4cdb36a0c71f
marjorieB <marjorie.bournat@wanadoo.fr>
2014-06-30 09:36:55 +0200

    Delete ServeurItf.java

--- src/centralise_1V1/ServeurItf.java
+++
@@ -1,8 +0,0 @@
-package centralise_1V1;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServeurItf extends Remote {
-	public void matchmaking(JoueurItf j) throws RemoteException;
-}
