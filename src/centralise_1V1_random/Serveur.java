04269f11ec276dfffd69adbb692fb618a8bceb50
marjorieB <marjorie.bournat@wanadoo.fr>
2014-07-01 11:51:56 +0200

    pour small

--- src/centralise_1V1_random/Serveur.java
+++ src/centralise_1V1_random/Serveur.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 
 import centralise_1V1_utilitaire.*;
